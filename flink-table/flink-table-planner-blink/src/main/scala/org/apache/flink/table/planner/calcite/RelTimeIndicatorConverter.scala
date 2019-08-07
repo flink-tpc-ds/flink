00e623ecfc5b12308b2ac39efc875c1d6312c36c
godfreyhe <godfreyhe@163.com>
2019-08-07 16:40:55 +0800

    [FLINK-13611] [table-planner-blink] fix "some AggregateCall attributes is lost in RelTimeIndicatorConverter"

--- flink-table/flink-table-planner-blink/src/main/scala/org/apache/flink/table/planner/calcite/RelTimeIndicatorConverter.scala
+++ flink-table/flink-table-planner-blink/src/main/scala/org/apache/flink/table/planner/calcite/RelTimeIndicatorConverter.scala
@@ -436,8 +436,11 @@ class RelTimeIndicatorConverter(rexBuilder: RexBuilder) extends RelShuttle {
       AggregateCall.create(
         call.getAggregation,
         call.isDistinct,
+        call.isApproximate,
+        call.ignoreNulls,
         call.getArgList,
         call.filterArg,
+        call.getCollation,
         callType,
         call.name)
     }
